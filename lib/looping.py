d9c4d657d73874000b3a6ddeaf08fa4534d8bbdd
Elijah <iamngunjiri@gmail.com>
2025-02-11 08:53:25 +0300

    Complete Control Flow: Loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # This is a good example of how we used list comprehension 
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while num <= 100:
+        if (num%3 == 0 and num%5 == 0):
+            print("FizzBuzz")
+        elif (num%3 == 0):
+            print("Fizz")
+        elif (num%5 == 0):
+            print("Buzz")
+        else:
+            print(num)
+        num += 1
